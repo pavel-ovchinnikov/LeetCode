fc88d7c76f6b8f0b66270d4cca83e5becac825b5
Pavel Ovchinnikov <povchinnikov.life@gmail.com>
2022-05-02 21:23:19 +0300

    add task 0001_two_sum

---
+++ Algorithms/0001_two_sum/two_sum.go
@@ -0,0 +1,15 @@
+package two_sum_0001
+
+func twoSum(nums []int, target int) []int {
+	index := make(map[int]int, len(nums))
+
+	for i, b := range nums {
+		if j, ok := index[target-b]; ok {
+			return []int{j, i}
+		}
+
+		index[b] = i
+	}
+
+	return nil
+}
